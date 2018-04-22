ebcb3863b89736e0dafb3ed8c0655d2c01b47b2e
nmt <nathaliamtan@gmail.com>
2018-04-22 18:09:45 +1000

    Leaaarrning

--- vue.js
+++ vue.js
@@ -1,6 +1,13 @@
-new Vue({
-    el: '#app',
+let app = new Vue({
+    el: '#app', // what element is this getting rendered into?
     data: {
       message: 'Hello Vue!'
     }
   })
+
+let app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'This is the current date! AND time! ' + new Date().toLocaleString()
+  }
+})
